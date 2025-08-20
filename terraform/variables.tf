6644d0906f49199c2ab617ee4769ec9fa8b90c3e
Sana Siddiqui <sana.siddiqui115@gmail.com>
2025-08-20 14:25:15 +0200

    changed root variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -34,3 +34,15 @@ variable "secondary_subnet_availability_zone" {
   type    = string
   default = "us-east-1b"
 }
+
+variable "root_domain"         { type = string, default = "sanasiddiquiaws.info" }
+variable "public_subnet_ids"   { type = list(string) }  # two public subnets (AZ A/B)
+variable "private_subnet_a_id" { type = string }        # vote instance subnet
+variable "private_subnet_b_id" { type = string }        # result instance subnet
+variable "vpc_id"              { type = string }
+# existing backend/private IPs
+variable "redis_host"          { type = string, default = "10.0.2.247" }
+variable "postgres_host"       { type = string, default = "10.0.2.190" }
+variable "postgres_user"       { type = string, default = "postgres" }
+variable "postgres_pass"       { type = string, default = "postgres" }
+variable "postgres_db"         { type = string, default = "postgres" }
